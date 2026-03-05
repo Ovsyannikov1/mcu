8181834ed87718489d34efa92af4bc5cc18f169f
Ovsyannikov1 <Ovsyannikovvadim@icloud.com>
2026-03-05 19:29:51 +0300

    main.c

--- Repositories/pico/mcu/01-blink/main.c
+++ Repositories/pico/mcu/01-blink/main.c
@@ -1,10 +1,12 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "pico/stdio_usb.h"
 
 const uint LED_PIN = 25;
 
 int main()
 {
+    stdio_usb_init();
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     
